2c9cb9c2095f5cdf383d63a2d94fc8c425539305
MathEiji <matheus.eiji@gmail.com>
2019-03-19 15:35:21 -0300

    Done

--- site/mod5-sol/js/script.js
+++ site/mod5-sol/js/script.js
@@ -116,13 +116,13 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl,"randomCategoryShortName",chosenCategoryShortName);
-      dc.loadMenuItems(chosenCategoryShortName);
+
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content","<section class='row'>" + homeHtmlToInsertIntoMainPage +"<section>");
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
